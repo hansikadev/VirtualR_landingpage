6cb9e09ffcaa3c1c9b43ce19b3672c242970d873
hansikadev <tanwarhansika123@gmail.com>
2025-03-18 23:26:00 +0530

    add Hero, Features, and Workflow sections to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Herosection from "./components/Herosection";
 import Navbar from "./components/Navbar";
+import Featuressection from "./components/Featuressection";
+import Workflow from "./components/Workflow";
 
 export default function App() {
   return (
     <>
       <Navbar/> 
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Herosection/>
+        <Featuressection/>
+        <Workflow/>
+
+      </div>
     </>
   )
 }
